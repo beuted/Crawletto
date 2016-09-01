cae5e42dafb76c5567ec082667fcc9b79fdb70f7
Benoit Jehanno <b.jehanno@criteo.com>
2016-09-01 14:32:22 +0200

    Fixing bower packages & dependency route issues

--- public/config.ts
+++ public/config.ts
@@ -6,11 +6,11 @@ requirejs.config({
     paths: {
         '_': 'bower_components/lodash/lodash',
         'Q': 'bower_components/q/q',
-        'easystarjs': 'bower_components/easystarjs/easystar-0.2.3.min',
+        'easystarjs': 'bower_components/easystarjs/bin/easystar-0.2.3.min',
         'phaser': 'vendors/phaser/phaser.min',
         'phaserPluginIsometric': 'vendors/phaser/phaser-plugin-isometric.min',
-        'text': 'bower_components/requirejs-plugins/text',
-        'json': 'bower_components/requirejs-plugins/json'
+        'text': 'bower_components/requirejs-plugins/lib/text',
+        'json': 'bower_components/requirejs-plugins/src/json'
     },
     shim: {
         'easystarjs': {
