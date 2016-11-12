6d74b0e61939912b0459d78103dad281c93800c7
Benoit deKajoo <dekajoo@gmail.com>
2016-11-12 16:34:39 +0100

    reordering public/typings/index.d.ts

--- public/typings/index.d.ts
+++ public/typings/index.d.ts
@@ -1,7 +1,6 @@
 /// <reference path="modules/lodash/index.d.ts" />
 /// <reference path="modules/requirejs/index.d.ts" />
-
+/// <reference path="typings_custom/easystarjs/easystarjs.d.ts" />
 /// <reference path="typings_custom/phaser.d.ts" />
 /// <reference path="typings_custom/phaser.plugin.isometric.d.ts" />
 /// <reference path="typings_custom/socket.io-client/socket.io-client.d.ts" />
-/// <reference path="typings_custom/easystarjs/easystarjs.d.ts" />
