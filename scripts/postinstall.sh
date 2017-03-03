3e850b2b23a0234736461c018b52d3f31f2d5ad5
Benoit Jehanno <b.jehanno@criteo.com>
2017-03-03 14:48:04 +0100

    remove typings

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 bower install
-
-cd ./server/
-../node_modules/.bin/typings install
-cd ..
-cd ./public/
-../node_modules/.bin/typings install
-cd ..
-
 ./node_modules/grunt-cli/bin/grunt default
