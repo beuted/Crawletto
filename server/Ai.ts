c079729f87c935138e5d8d43fd54ad2d6a0d9c66
Benoit Jehanno <b.jehanno@criteo.com>
2017-04-07 19:08:24 +0200

    Improve AI being aggressive or not

--- server/Ai.ts
+++ server/Ai.ts
@@ -1,16 +1,31 @@
 import * as _ from 'lodash';
 import * as Geo from './utils/Geo';
-import { Move, Attack } from './Action';
+import { Move, Attack, IAction } from './Action';
 import { Character } from './Character';
 import { GameEventHandler } from './GameEventHandler';
 
+var config = require('../public/shared/config');
+
 export class Ai extends Character {
+    private isAggressive: boolean = false;
 
     constructor(gridPosition: Geo.IPoint, mapPosition: Geo.IPoint, type: string) {
         super(gridPosition, mapPosition, type);
+
+        this.isAggressive = config.characters[type].isAggressive;
     }
 
     public calculateNextAction() {
+        let action: IAction;
+        if (this.isAggressive)
+            action = this.getAggressiveAction();
+        else
+            action = this.getPeacefulAction();
+            
+        this.planAction(action);
+    }
+
+    public getAggressiveAction(): IAction {
         var characterOnMap = GameEventHandler.playersCollection.getAllOnMap(this.mapPosition);
 
         var minDist = Number.MAX_VALUE;
@@ -25,8 +40,7 @@ export class Ai extends Character {
 
         // If a player is just next to you, attack him
         if (closestChar && Geo.Tools.distance(closestChar.gridPosition, this.gridPosition) == 1) {
-            this.planAction(new Attack(closestChar.guid, this.guid));
-            return;
+            return new Attack(closestChar.guid, this.guid);
         }
 
         var path: {x: number, y: number}[] = null;
@@ -35,14 +49,16 @@ export class Ai extends Character {
         }
         
         // If we can't find a way to a player just randomly walk
-        if (!path || !path[1]) {
-            var XorY: boolean = !!Math.round(Math.random());
-            var moveDirection = Math.floor(Math.random() * 3) - 1;
-            this.planAction(new Move({ x: this.gridPosition.x + (XorY ? moveDirection : 0), y: this.gridPosition.y + (!XorY ? moveDirection : 0) }));
-            return;
-        }
+        if (!path || !path[1])
+            return this.getPeacefulAction();
 
         // Else way to the nearest player
-        this.planAction(new Move({ x: path[1].x, y: path[1].y }));
+        return new Move({ x: path[1].x, y: path[1].y });
+    }
+
+    public getPeacefulAction(): IAction {
+        var XorY: boolean = !!Math.round(Math.random());
+        var moveDirection = Math.floor(Math.random() * 3) - 1;
+        return new Move({ x: this.gridPosition.x + (XorY ? moveDirection : 0), y: this.gridPosition.y + (!XorY ? moveDirection : 0) });
     }
 }
